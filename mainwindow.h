d7767be65bd91290f9f5416e6218ecca93138579
Nocs782 <emailforuniversity99@gmail.com>
2025-02-23 03:26:39 +0500

    feat: add expected fucntions to .h file

--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void fetchWeather();
+    void handleResponse();
+    void loadWeatherIcon(QString iconCode);
+    QString capitalizeWords(const QString &input);
 
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
 };
-#endif // MAINWINDOW_H
+
+#endif
